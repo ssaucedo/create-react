3dd2d07d9b08862bdbdcadebdd2b5d1035979e73
Santiago Saucedo <santiago.g.saucedo@gmail.com>
2017-08-20 15:37:06 -0300

    (feat) Add babel rc + webpack config in ES6

---
+++ webpack.config.babel.js
@@ -0,0 +1,63 @@
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import CleanWebpackPlugin from 'clean-webpack-plugin'
+import path from 'path'
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'dist'),
+}
+
+export default {
+  entry: [
+    PATHS.app,
+  ],
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  stats: {
+    colors: true,
+    reasons: true, // if fail, show it very verbose
+  },
+  resolve: {
+    alias: {
+      config: path.resolve(__dirname, 'app/config/'),
+      helpers: path.resolve(__dirname, 'app/helpers/'),
+      modules: path.resolve(__dirname, 'app/modules/'),
+    },
+  },
+  module: {
+    rules: [
+      { test: /\.js?$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'babel-loader' }],
+      },
+      //{ test: /\.css$/,
+      //  loader: ExtractTextPlugin.extract({
+      //    loader: 'css-loader?importLoaders=1!postcss-loader',
+      //   }),
+      //},
+      { test: /\.ico$/,
+        use: [{ loader: 'file?name=[name].[ext]&context=./app/' }],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: PATHS.build,
+    hot: true,
+    inline: true,
+    quiet: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(['build'], {}),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve('./app/index.ejs'),
+      inject: true,
+    }),
+  ],
+}
